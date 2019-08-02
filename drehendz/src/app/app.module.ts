58208a8d2648cefd28d5a05ba2872e853189d438
Dre <drehendz@dwaynes-MacBook-Pro.local>
2019-08-02 18:51:33 -0500

    updtaes

---
+++ drehendz/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { CartService } from './products/services/cart.service';
+import {AppComponent} from './app.component';
+import {AdminComponent} from './admin/admin.component';
+import {AdminProductsComponent} from './admin/admin-products/admin-products.component';
+import {AdminUsersComponent} from './admin/admin-users/admin-users.component';
+import {ProductsComponent} from './products/products.component';
+import {CartComponent} from './products/cart/cart.component';
+import {UserComponent} from './user/user.component';
+import {SignInComponent} from './user/sign-in/sign-in.component';
+import {SignUpComponent} from './user/sign-up/sign-up.component';
+import { AppRoutingModule } from './app-routing.module';
+  
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminComponent,
+    AdminProductsComponent,
+    AdminUsersComponent,
+    ProductsComponent,
+    CartComponent,
+    UserComponent,
+    SignInComponent,
+    SignUpComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [CartService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
