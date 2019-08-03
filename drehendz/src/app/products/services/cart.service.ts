919259a4882c97968f0b10c4c658519c99474337
Dre <drehendz@dwaynes-MacBook-Pro.local>
2019-08-03 11:03:01 -0500

    fix bug in store.ts

--- drehendz/src/app/products/services/cart.service.ts
+++ drehendz/src/app/products/services/cart.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
-import { UPDATE_CART } from 'src/app/redux/actions/action';
-import { ProductModel } from 'src/app/models/product.model';
+
 
 
 @Injectable({
--- drehendz/src/app/redux/store.ts
+++ drehendz/src/app/redux/store.ts
@@ -7,22 +7,3 @@ import {UPDATE_CART, ADD_TO_CART} from './actions/action';
     
 
 
-export const INITIAL_STATE : IAppState = 
-{
-    products : new Array<ProductModel>()
-};
-
-export function rootReducer (state: IAppState, action: AnyAction) :
- IAppState 
- {
-     switch(action.type) 
-     {
-         case ADD_TO_CART:
-             return Object.assign({}, state, action.payload);
-
-
-         case UPDATE_CART:
-             return Object.assign({}, state, action.payload);
-     }
-     return state;
- }
