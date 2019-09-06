5aa06b47fef3f089915cd7f0633352dbad4f767f
Dre <drehendz@dwaynes-MacBook-Pro.local>
2019-09-05 23:44:46 -0500

    using data binding and router link

--- drehendz/src/app/user/sign-up/sign-up.component.ts
+++ drehendz/src/app/user/sign-up/sign-up.component.ts
@@ -2,14 +2,29 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sign-up',
-  templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.css']
+  template: `
+  <h2>Welcome {{name}}  </h2>
+   <button (click)="greeting='welcome to the hendz'">Click me</button>
+  {{greeting}}
+
+  `,
+  styles: []
 })
 export class SignUpComponent implements OnInit {
+ 
+ 
+  public name = "dre";
+  public greeting ="";
+
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit()
+   {
+  }
+  onClick()
+  {
+    console.log('welcome to the hendz');
   }
 
 }
