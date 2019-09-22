3f537392ec950bfc636c6c6ef786283621659af9
Dre <drehendz@dwaynes-MacBook-Pro.local>
2019-09-22 11:23:39 -0500

    fix routes

--- drehendz/src/app/app.routes.ts
+++ drehendz/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { ModuleWithProviders } from '@angular/compiler/src/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SignInComponent } from './user/sign-in/sign-in.component';
 
 export const router:  Routes = 
 [
