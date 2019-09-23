6cfaaf0412ad1b01f7e09bcd04c3b03f279c0ea1
Dre <drehendz@dwaynes-MacBook-Pro.local>
2019-09-22 20:27:08 -0500

    index routing

--- drehendz/src/app/index/index.routing.ts
+++ drehendz/src/app/index/index.routing.ts
@@ -1,11 +1,12 @@
 import {IndexComponent} from "./index.component";
 import {Routes} from "@angular/router";
+import { LoginComponent } from './login/login.component';
 
 
 export const IndexRoutes: Routes = 
 [
     {path:"", children: [ 
         {path:"", component: IndexComponent},
-        {path:"", }
+        {path:"login", component: LoginComponent  }
     ] }
-]
+];
