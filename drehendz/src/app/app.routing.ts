c81293844d655d06a8c0347c25bc0692d716482a
Dre <drehendz@dwaynes-MacBook-Pro.local>
2019-09-22 20:57:41 -0500

    app routing

---
+++ drehendz/src/app/app.routing.ts
@@ -0,0 +1,17 @@
+import{Routes} from '@angular/router';
+import {IndexComponent} from './index/index.component';
+
+
+export const AppRoutes: Routes = [
+
+    {path: '', children:  [
+        {path:'', loadChildren: './index/index.module#IndexModule'},
+        {path: 'products',
+    loadChildren:  './layouts/product/product.module#ProductModule'},
+    {
+        path: 'users',
+        loadChildren: './layouts/user/user.module#UserModule'
+    },
+    ]
+}
+]
