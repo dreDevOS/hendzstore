1a0288f825b6d8e0a838710c3e497193f6cfe933
Dre <drehendz@dwaynes-MacBook-Pro.local>
2019-10-09 16:34:17 -0500

    test

--- drehendz/src/app/index/login/login.component.ts
+++ drehendz/src/app/index/login/login.component.ts
@@ -1,17 +1,18 @@
-import { Component, OnInit, ɵɵsanitizeUrlOrResourceUrl } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import {NgForm, EmailValidator} from "@angular/forms";
 import { AuthService } from 'src/app/auth/auth.service';
 import { UserService } from 'src/app/shared/services/user.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { User } from 'src/app/shared/models/user';
 import {ToastrService} from "../../shared/services/toastr.service";
-import { router } from 'src/app/app.routes';
 declare var $: any;
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  providers: [EmailValidator]
+  
 })
 export class LoginComponent implements OnInit {
 
@@ -47,14 +48,19 @@ export class LoginComponent implements OnInit {
                                       verified_email: res.user.emailVerified,
                                       phoneNumber: res.user.phoneNumber,
                                     picture: res.user.photoURL};
-                                  this.userService.createUser(user);
-                                this.toastService.success("Registering", "User Registerion");
-                              setTimeout((router: Router)   => {
+
+         this.userService.createUser(user);
+
+     this.toastService.success("Registering", "User Registerion");
+                              
+                                setTimeout((router: Router)   => {
                                 $("#createdUserForm").modal("hide");
                                 this.router.navigate(["/"]);
 
                               }, 1500);
-         }).catch((err)  =>{this.errorInUserCreate = true;
+
+         })
+         .catch((err)  =>{this.errorInUserCreate = true;
         this.errorMessage = err;
       this.toastService.error("Error while Creating User", err);});
   }
@@ -63,16 +69,22 @@ export class LoginComponent implements OnInit {
   signInWithEmail(userForm: NgForm){
     this.authService
     .signInRegular(userForm.value["emailId"], userForm.value["loginPassword"])
-    .then((res)   => {this.toastService.success("Authentication Success", "Logging in please wait"); 
+    .then((res)   => {
+      
+      this.toastService.success("Authentication Success", "Logging in please wait"); 
+
    const returnUrl = this.route.snapshot.queryParamMap.get("returnUrl");
 
    setTimeout((router: Router)  => {this.router.navigate([returnUrl  || "/"]);
   }, 1500);
   this.router.navigate(["/"]);
   })
-  .catch((err) => {this.toastService.error("Authentication Failed", "Invalid Credentials, Please check your credentials");
+  .catch((err) => {
+    this.toastService.error("Authentication Failed", "Invalid Credentials, Please check your credentials");
 });
   }
+
+
 signInWithGoogle(){
   this.authService 
   .signInWithGoogle()
--- drehendz/src/app/shared/services/toastr.service.ts
+++ drehendz/src/app/shared/services/toastr.service.ts
@@ -9,20 +9,19 @@ export class ToastrService {
   constructor() { }
 
 success(title,msg){
-  toastr.success(msg, title)
+  toastr.success(msg, title);
 }
-
-info(title, msg){
-  toastr.info(msg, title)
+info(title,msg) {
+  toastr.info(msg, title);
 }
- warning(title, msg){
-   toastr.warning(msg, title)
- }
- error(title, msg){
-   toastr.error(msg, title)
- }
+warning(title, msg) {
+  toastr.warning(msg, title);
+}
+error(title,msg){
+  //toastr.error(msg, title);
+}
+wait(title, msg) {
+  toastr.info(msg, title, {timeOut: 3000 });
 
- wait(title, msg){
-   toastr.error(msg, title);
- }
+}
 }
