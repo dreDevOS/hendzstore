f23cbe0f6418c50cf3d30ae60e6505799ac0d127
Dre <drehendz@dwaynes-MacBook-Pro.local>
2019-10-11 22:23:56 -0500

    convert app component to scss

--- drehendz/src/app/app.component.ts
+++ drehendz/src/app/app.component.ts
@@ -6,7 +6,7 @@ declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent 
 {
