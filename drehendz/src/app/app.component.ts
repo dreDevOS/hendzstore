ac43d741454185862a7d6eba09253d4a0b9d8362
Dre <drehendz@dwaynes-MacBook-Pro.local>
2019-10-13 12:38:12 -0500

    fix product list

--- drehendz/src/app/app.component.ts
+++ drehendz/src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from './shared/services/user.service';
+import {fadeAnimation} from "./shared/animations/fadeIntRoute";
+
 
 declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [fadeAnimation]
 })
 export class AppComponent 
 {
